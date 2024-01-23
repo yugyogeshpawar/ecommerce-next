3cf56886572a29d307a0d2ab5e6dde652dc52c63
iambhavesh09 <bhaveshjhawar09@gmail.com>
2024-01-23 20:53:08 +0530

    Home Page Render To Furniture Page

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
-// const { i18n } = require("./next-i18next.config");
-
 module.exports = {
-  // i18n,
+  // i18n, // Uncomment or remove this line if necessary
   devIndicators: {},
   publicRuntimeConfig: {
     // Available on both server and client
     theme: "DEFAULT",
     currency: "USD",
-  },
+  }
+  
+
 };
-
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
-
-// module.exports = withBundleAnalyzer({
-//   i18n,
-//   devIndicators: {},
-//   publicRuntimeConfig: {
-//     // Available on both server and client
-//     theme: "DEFAULT",
-//     currency: "USD",
-//   },
-// });
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,51 @@
-import { useState } from "react";
-import Box from "@mui/material/Box";
-import Setting from "components/Setting";
-import Footer from "pages-sections/landing/Footer";
-import Section1 from "pages-sections/landing/Section1";
-import Section2 from "pages-sections/landing/Section2";
-import Section3 from "pages-sections/landing/Section3";
-import Section4 from "pages-sections/landing/Section4";
-import Section6 from "pages-sections/landing/Section6";
-import Section5 from "pages-sections/landing/Section5";
+// import { useState } from "react";
+// import Box from "@mui/material/Box";
+// import Setting from "components/Setting";
+// import Footer from "pages-sections/landing/Footer";
+// import Section1 from "pages-sections/landing/Section1";
+// import Section2 from "pages-sections/landing/Section2";
+// import Section3 from "pages-sections/landing/Section3";
+// import Section4 from "pages-sections/landing/Section4";
+// import Section6 from "pages-sections/landing/Section6";
+// import Section5 from "pages-sections/landing/Section5";
+// const IndexPage = () => {
+//   const [filterDemo, setFilterDemo] = useState("");
+//   return (
+//     <Box id="top" overflow="hidden" bgcolor="background.paper">
+//       <Section1 />
+//       <Section6 setFilterDemo={setFilterDemo} />
+//       <Section2 />
+//       <Section5 />
+//       <Section3 filterDemo={filterDemo} setFilterDemo={setFilterDemo} />
+//       <Section4 />
+//       <Footer />
+//       <Setting />
+//     </Box>
+//   );
+// };
+// export default IndexPage;
+
+
+
+// pages/index.js
+import { useEffect } from 'react';
+
 const IndexPage = () => {
-  const [filterDemo, setFilterDemo] = useState("");
-  return (
-    <Box id="top" overflow="hidden" bgcolor="background.paper">
-      <Section1 />
-      <Section6 setFilterDemo={setFilterDemo} />
-      <Section2 />
-      <Section5 />
-      <Section3 filterDemo={filterDemo} setFilterDemo={setFilterDemo} />
-      <Section4 />
-      <Footer />
-      <Setting />
-    </Box>
-  );
+  useEffect(() => {
+    // Redirect to the desired URL
+    window.location.replace('/furniture-shop');
+  }, []);
+
+  // This component won't be rendered due to the redirect
+  return <div>Redirecting...</div>;
 };
+
+export const getServerSideProps = async ({ res }) => {
+  // Set up server-side redirect
+  res.writeHead(302, { Location: '/furniture-shop' });
+  res.end();
+
+  return { props: {} };
+};
+
 export default IndexPage;
